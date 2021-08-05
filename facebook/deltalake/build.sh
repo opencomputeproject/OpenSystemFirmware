ba8c6f41d8f940a7f2f11a361ee70c1257981dee
Christian Walter <christian.walter@9elements.com>
2021-08-05 19:02:09 +0200

    Initial commit for the OCP Delta Lake build scripts.
    Signed-off-by: Christian Walter <christian.walter@9elements.com>

---
+++ facebook/deltalake/build.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+GOLANG_URL="https://golang.org/dl/go1.15.14.linux-amd64.tar.gz"
+
+echo "Fetching dependencies"
+
+pushd src
+
+if [ ! -d "initramfs" ]; then
+    mkdir initramfs
+fi
+
+pushd initramfs
+if [ ! -d "go" ]; then
+    wget "${GOLANG_URL}"
+    tar -xf go1.15.14.linux-amd64.tar.gz
+fi
+
+export GOROOT="${PWD}/go"
+export GOPATH="${PWD}/gopath"
+
+if [ ! -d "gopath" ]; then
+    mkdir gopath
+fi
+
+go/bin/go get github.com/u-root/u-root
+
+echo "Building u-root"
+go/bin/go build github.com/u-root/u-root
+
+./u-root -build=bb -o payload.cpio core boot
+
+xz payload.cpio
+
+popd # initramfs
+popd # src
+
+cp src/initramfs/payload.cpio.xz bin/deltalake/.
+
+if [ ! -d "src/coreboot" ]; then
+    git clone https://review.coreboot.org/coreboot src/coreboot
+fi
+
+echo "Building coreboot"
+
+# Copy the configuration - build the toolchain and build coreboot.
+cp bin/deltalake/defconfig src/coreboot/configs/defconfig
+mkdir src/coreboot/site-local
+
+cp -R bin/deltalake src/coreboot/site-local/.
+
+pushd src
+pushd coreboot
+
+make crossgcc-i386 CPUS=$(nproc)
+make defconfig
+make
+
+popd # coreboot
+popd # src
