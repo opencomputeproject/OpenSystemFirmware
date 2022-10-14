d792cace68152ddde4bcf5dff079fb5bc6e5d545
Eddie Vas <aeddiesharma@fb.com>
2022-10-14 00:08:25 -0400

    Wiwynn/DeltaLake: Update FSP to latest release of CedarIsland for FSP 2.2.0.33A
    This change updates FSP to the latest CedarIsland FSP which gets
    the CedarIsland FSP updated to version 2.2.0.33A amongst other
    changes. Instead of providing a split script in the download and
    build script, we added FSP as a proper 3rd party repo link within
    coreboot like how we are doing for u-root, kernel etc.
    
    Tested: Booted DeltaLake to Target OS
    
    Signed-off-by: Eddie Vas <aeddiesharma@fb.com>

--- Wiwynn/deltalake/download_and_build.sh
+++ Wiwynn/deltalake/download_and_build.sh
@@ -2,7 +2,6 @@
 
 scriptdir="$(realpath "$(dirname "$0")")"
 osf_builder_hash="11d894bfe70e9b371c7dba9a02c6ccb24bd0c524"
-fsp_hash="0e18518131e7ecfeeb4afeab306121ca554c5054"
 edk2_non_osi_hash="61662e8596dd9a64e3372f9a3ba6622d2628607c"
 
 patches="${scriptdir}/src/*.patch"
@@ -25,11 +24,6 @@ download_intel_blobs()
   cd edk2-non-osi
   git checkout $edk2_non_osi_hash
   cd ..
-  git clone https://github.com/intel/FSP.git
-  cd FSP
-  git checkout $fsp_hash
-  mkdir CPX-FSP-split
-  python Tools/SplitFspBin.py split -f CedarIslandFspBinPkg/Fsp.fd -o CPX-FSP-split
   cd $scriptdir
 }
 
@@ -42,9 +36,6 @@ cd osf-builder
 git checkout $osf_builder_hash
 # Apply patches that add Delta Lake files to osf-builder
 apply_patches
-cp ${scriptdir}/bin/FSP/CPX-FSP-split/Fsp_M.fd projects/deltalake/blobs/Server_M.fd
-cp ${scriptdir}/bin/FSP/CPX-FSP-split/Fsp_S.fd projects/deltalake/blobs/Server_S.fd
-cp ${scriptdir}/bin/FSP/CPX-FSP-split/Fsp_T.fd projects/deltalake/blobs/Server_T.fd
 cp ${scriptdir}/bin/edk2-non-osi/Silicon/Intel/WhitleySiliconBinPkg/CpxMicrocode/mBF5065B_07002302.mcb projects/deltalake/blobs/microcode.mcb
 cp ${scriptdir}/bin/edk2-non-osi/Platform/Intel/WhitleyOpenBoardBinPkg/Ifwi/DeltaLake/FlashDescriptor.bin projects/deltalake/blobs/flashregion_0_flashdescriptor.bin
 cp ${scriptdir}/bin/edk2-non-osi/Platform/Intel/WhitleyOpenBoardBinPkg/Ifwi/DeltaLake/Me.bin projects/deltalake/blobs/flashregion_2_intel_me.bin
